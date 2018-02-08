e89c18e9d1418bbe5105a6ab5bf608b1207075b4
Dominic Bartel <DominicBartel@gmail.com>
2018-02-08 13:59:47 -0500

    worked on VendingMachine class, added getitematslot method

--- Capstone/Classes/VendingMachine.cs
+++ Capstone/Classes/VendingMachine.cs
@@ -9,11 +9,47 @@ namespace Capstone.Classes
 {
     public class VendingMachine
     {
-        public Dictionary<string, VendingMachineItem> Inventory { get; set; }
+        public Dictionary<string, List<VendingMachineItem>> Inventory { get; set; }
 
         public decimal Balance { get; private set; }
 
-        public string[] Slots { get; }
+        public string[] Slots
+        {
+            get
+            {
+                string[] slots = new string[0];
+
+                return slots;
+
+            }
+
+        }
+
+        public void FeedMoney(int dollars)
+        {
+            Balance += dollars;
+        }
+
+        public VendingMachineItem GetItemAtSlot(string slot)
+        {
+            
+            VendingMachineItem tempItem = Inventory[slot][0];
+            return tempItem;
+        }
+
+        public int GetQuantityRemaining(string slot)
+        {
+
+            return Inventory[slot].Count;
+        }
+
+        public VendingMachine(Dictionary<string, List<VendingMachineItem>> startingInventory)
+        {
+            foreach(string key in startingInventory.Keys)
+            {
+                Inventory[key] = startingInventory[key];
+            }
+        }
 
     }
 }
